ea99ac9647cf48bae6cb641dcbe8e0f04dbbfc33
Tim Jedrek <tim@timjedrek.com>
2024-01-28 19:26:06 -0600

    start on nav

--- src/navigation.js
+++ src/navigation.js
@@ -3,118 +3,180 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Home',
+      href: getPermalink('/')
+    },
+    {
+      text: 'Features',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: 'CRM',
+          href: getPermalink('/')
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
+          text: 'Automation',
+          href: getPermalink('/')
         },
         {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
+          text: 'Call Tracking and SMS Marketing',
+          href: getPermalink('/')
         },
         {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
+          text: 'Email Marketing',
+          href: getPermalink('/')
         },
       ],
     },
     {
-      text: 'Pages',
+      text: 'Support',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
+          text: 'FAQ',
+          href: getPermalink('/')
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
+          text: 'Documentation',
+          href: getPermalink('/')
         },
         {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
+          text: 'Support Ticket',
+          href: getPermalink('/')
         },
       ],
     },
     {
-      text: 'Landing',
+      text: 'About',
       links: [
         {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
+          text: 'Who We Are',
+          href: getPermalink('/')
         },
         {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
+          text: 'Right Rudder Marketing',
+          href: getPermalink('/')
         },
         {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
+          text: 'Blog',
+          href: getPermalink('/')
         },
       ],
     },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
-    },
+
   ],
+  // links: [
+  //   {
+  //     text: 'Homes',
+  //     links: [
+  //       {
+  //         text: 'SaaS',
+  //         href: getPermalink('/homes/saas'),
+  //       },
+  //       {
+  //         text: 'Startup',
+  //         href: getPermalink('/homes/startup'),
+  //       },
+  //       {
+  //         text: 'Mobile App',
+  //         href: getPermalink('/homes/mobile-app'),
+  //       },
+  //       {
+  //         text: 'Personal',
+  //         href: getPermalink('/homes/personal'),
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     text: 'Pages',
+  //     links: [
+  //       {
+  //         text: 'Features (Anchor Link)',
+  //         href: getPermalink('/#features'),
+  //       },
+  //       {
+  //         text: 'Services',
+  //         href: getPermalink('/services'),
+  //       },
+  //       {
+  //         text: 'Pricing',
+  //         href: getPermalink('/pricing'),
+  //       },
+  //       {
+  //         text: 'About us',
+  //         href: getPermalink('/about'),
+  //       },
+  //       {
+  //         text: 'Contact',
+  //         href: getPermalink('/contact'),
+  //       },
+  //       {
+  //         text: 'Terms',
+  //         href: getPermalink('/terms'),
+  //       },
+  //       {
+  //         text: 'Privacy policy',
+  //         href: getPermalink('/privacy'),
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     text: 'Landing',
+  //     links: [
+  //       {
+  //         text: 'Lead Generation',
+  //         href: getPermalink('/landing/lead-generation'),
+  //       },
+  //       {
+  //         text: 'Long-form Sales',
+  //         href: getPermalink('/landing/sales'),
+  //       },
+  //       {
+  //         text: 'Click-Through',
+  //         href: getPermalink('/landing/click-through'),
+  //       },
+  //       {
+  //         text: 'Product Details (or Services)',
+  //         href: getPermalink('/landing/product'),
+  //       },
+  //       {
+  //         text: 'Coming Soon or Pre-Launch',
+  //         href: getPermalink('/landing/pre-launch'),
+  //       },
+  //       {
+  //         text: 'Subscription',
+  //         href: getPermalink('/landing/subscription'),
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     text: 'Blog',
+  //     links: [
+  //       {
+  //         text: 'Blog List',
+  //         href: getBlogPermalink(),
+  //       },
+  //       {
+  //         text: 'Article',
+  //         href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+  //       },
+  //       {
+  //         text: 'Article (with MDX)',
+  //         href: getPermalink('markdown-elements-demo-post', 'post'),
+  //       },
+  //       {
+  //         text: 'Category Page',
+  //         href: getPermalink('tutorials', 'category'),
+  //       },
+  //       {
+  //         text: 'Tag Page',
+  //         href: getPermalink('astro', 'tag'),
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     text: 'Widgets',
+  //     href: '#',
+  //   },
+  // ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
